0f075ae0f45fe0810df0cbc3a19da6bc52d63689
Quoc-Bao-213 <taquocbao.it.99@gmail.com>
2021-05-10 10:42:04 +0700

    Config express, move file html to handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const handlebars = require('express-handlebars')
+const path = require('path')
+
 const app = express();
-const port = 3000;
+const port = 3030;
+
+// Lookup folder public
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Config middleware method post
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+app.use(express.json())
+
+// Template engine
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    })
+)
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home');
 });
 
 app.listen(port, () => {
